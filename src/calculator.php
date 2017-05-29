443189d3f7c9f01ba0271072bfd236077626b369
nantnan <am_amp00@msn.com>
2017-05-29 15:42:55 +0700

    first commit

---
+++ src/calculator.php
@@ -0,0 +1,26 @@
+<?php 
+    namespace  nantnan\calculator;
+    class Calculator
+    {
+        public function add($f_num, $sec_num)
+        {
+            return $f_num + $sec_num;
+        } 
+         public function subtrac($f_num, $sec_num)
+         {
+            return $f_num - $sec_num;
+        } 
+        public function multiply($f_num, $sec_num)
+        {
+            return $f_num * $sec_num;
+        } 
+        public function divide($f_num, $sec_num)
+        {
+            return $f_num / $sec_num;
+        } 
+       
+
+
+
+
+    }
